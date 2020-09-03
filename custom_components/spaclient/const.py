e092d6baf81d7b7f5ac786f2c554e3404ab61162
Nathan Spencer <natekspencer@gmail.com>
2020-09-03 13:46:03 -0600

    use spaclient as domain

--- custom_components/spaclient/const.py
+++ custom_components/spaclient/const.py
@@ -9,7 +9,7 @@ from homeassistant.components.climate.const import (
 )
 from homeassistant.components.fan import SPEED_HIGH, SPEED_LOW, SPEED_OFF
 
-DOMAIN = "balboa"
+DOMAIN = "spaclient"
 BALBOA_PLATFORMS = ["climate", "switch", "binary_sensor", "fan"]
 CLIMATE_SUPPORTED_MODES = [HVAC_MODE_HEAT, HVAC_MODE_OFF]
 CLIMATE_SUPPORTED_FANSTATES = [FAN_OFF, FAN_LOW, FAN_MEDIUM, FAN_HIGH]
