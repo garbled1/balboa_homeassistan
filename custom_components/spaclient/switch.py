59596666f85253787a5cb851aa07142d31ef9e16
Nathan Spencer <natekspencer@gmail.com>
2020-09-03 14:56:56 -0600

    add temperature range switch

--- custom_components/spaclient/switch.py
+++ custom_components/spaclient/switch.py
@@ -1,7 +1,7 @@
 """Support for Balboa Spa switches."""
 import logging
 
-from homeassistant.components.switch import DEVICE_CLASS_SWITCH, SwitchDevice
+from homeassistant.components.switch import DEVICE_CLASS_SWITCH, SwitchEntity
 from homeassistant.const import CONF_NAME
 
 from . import BalboaEntity
@@ -16,6 +16,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
     device = entry.data[CONF_NAME]
     devs = []
 
+    devs.append(BalboaSpaSwitch(hass, spa, device, "Temp Range"))
     for num, value in enumerate(spa.light_array, 1):
         if value:
             devs.append(BalboaSpaSwitch(
@@ -30,7 +31,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
     async_add_entities(devs, True)
 
 
-class BalboaSpaSwitch(BalboaEntity, SwitchDevice):
+class BalboaSpaSwitch(BalboaEntity, SwitchEntity):
     """Representation of a Balboa Spa switch device."""
 
     def __init__(self, hass, client, device, entity, num=None):
@@ -50,6 +51,8 @@ class BalboaSpaSwitch(BalboaEntity, SwitchDevice):
         """Return True if the switch is on."""
         if self._entity == 'Mister':
             return self._client.get_mister()
+        elif self._entity == 'Temp Range':
+            return self._client.get_temprange()
         return self.getdata[self._entity](self._num-1)
 
     @property
@@ -60,20 +63,29 @@ class BalboaSpaSwitch(BalboaEntity, SwitchDevice):
     @property
     def icon(self):
         """Return the icon to use in the frontend, if any."""
-        if self._entity == 'Mister':
-            return "mdi:weather-fog"
         if self._entity == 'Light':
             return "mdi:lightbulb"
-        return "mdi:flash"
+        elif self._entity == 'Mister':
+            return "mdi:weather-fog"
+        elif self._entity == 'Temp Range':
+            return "mdi:thermometer-plus"
+        else:
+            return "mdi:flash"
 
     async def async_turn_off(self, **kwargs):
         """Turn off the switch."""
         if self._entity == 'Mister':
-            return self._client.change_mister(self._client.OFF)
-        await self.switch_change[self._entity](self._num-1, self._client.OFF)
+            return await self._client.change_mister(self._client.OFF)
+        elif self._entity == 'Temp Range':
+            return await self._client.change_temprange(self._client.TEMPRANGE_LOW)
+        else:
+            return await self.switch_change[self._entity](self._num-1, self._client.OFF)
 
     async def async_turn_on(self, **kwargs):
         """Turn on the switch."""
         if self._entity == 'Mister':
             return self._client.change_mister(self._client.ON)
-        await self.switch_change[self._entity](self._num-1, self._client.ON)
+        elif self._entity == 'Temp Range':
+            return await self._client.change_temprange(self._client.TEMPRANGE_HIGH)
+        else:
+            return await self.switch_change[self._entity](self._num-1, self._client.ON)
