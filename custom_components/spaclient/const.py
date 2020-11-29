580a3700a9316c564a192908945879edf04b9e68
Nathan Spencer <natekspencer@gmail.com>
2020-11-29 15:52:05 -0700

    updates to climate so that "Ready in Rest" is only reported and not able to be set

--- custom_components/spaclient/const.py
+++ custom_components/spaclient/const.py
@@ -1,12 +1,12 @@
 """Constants for the Balboa Spa Client integration."""
 from homeassistant.components.climate.const import (FAN_HIGH, FAN_LOW,
                                                     FAN_MEDIUM, FAN_OFF,
-                                                    HVAC_MODE_AUTO,
-                                                    HVAC_MODE_HEAT)
+                                                    HVAC_MODE_HEAT,
+                                                    HVAC_MODE_OFF)
 from homeassistant.components.fan import SPEED_HIGH, SPEED_LOW, SPEED_OFF
 
 CLIMATE_SUPPORTED_FANSTATES = [FAN_OFF, FAN_LOW, FAN_MEDIUM, FAN_HIGH]
-CLIMATE_SUPPORTED_MODES = [HVAC_MODE_HEAT, HVAC_MODE_AUTO]
+CLIMATE_SUPPORTED_MODES = [HVAC_MODE_HEAT, HVAC_MODE_OFF]
 CONF_SYNC_TIME = "sync_time"
 DEFAULT_SYNC_TIME = False
 DOMAIN = "spaclient"
